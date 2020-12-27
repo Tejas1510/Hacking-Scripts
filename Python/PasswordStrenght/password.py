b0366421dee738bdafa0bdda8473b2ba8f604f78
rutvik padhiyar <76583071+rutvikpadhiyar000@users.noreply.github.com>
2020-12-27 20:30:51 +0530

    Update password.py

--- Python/PasswordStrenght/password.py
+++ Python/PasswordStrenght/password.py
@@ -1,25 +1,34 @@
 import re
 p = input("Please enter a passowrd for checking its Strenght : ")
-count=0
-if(len(p)>=6):
-    count=count+1
-if(re.search("[a-z]",p)):
-    count=count+1
-if(re.search("[A-Z]",p)):
-    count=count+1
-if(re.search("[$#@]",p)):
-    count=count+1
-if(re.search("[0-9]",p)):
-    count=count+1
-if(count==1):
-    print("The Password is Poor")
-elif(count==2):
-    print("The Password is Fair")
-elif(count==3):
-    print("The Password is Good")
-elif(count==4):
-    print("The Password is Strong")
-elif(count==5):
-    print("The Password is Very Strong")
-else:
-    print("INVALID PASSWORD")
+
+password_strength_check(p):
+    length=len(p)
+    count=0
+    if length < 6:
+        return 1
+    else:
+        if 8<length <10:
+            count+=1
+        elif length <12:
+            count+=2
+        elif length <14:
+            count+=3
+        else:
+            count+=4
+
+        if(re.search("[a-z]",p)):
+            count=count+1
+        if(re.search("[A-Z]",p)):
+            count=count+1
+        if(re.search("[$#@]",p)):
+            count=count+1
+        if(re.search("[0-9]",p)):
+            count=count+1
+    return count
+
+pass_strength = {1:"very weak", 2:"weak", 3:"fair", 4:"strong"}
+
+try:
+    print("Your password is", pass_strength[count])
+except:
+    print("Your password is very strong")
