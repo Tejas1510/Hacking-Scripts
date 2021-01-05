121373b3baa166ec9964d40ac7f97a6f613518f9
saiharsha-22 <harshavardhangood@gmail.com>
2021-01-05 12:36:09 +0530

    Added script

---
+++ Python/ScreenShot Taker/app.py
@@ -0,0 +1,18 @@
+import pyautogui   
+import keyboard
+from tkinter import filedialog
+
+
+while True:
+    try:
+        if keyboard.is_pressed('p'):
+               screenshots = pyautogui.screenshot()
+               file_path = filedialog.asksaveasfilename(defaultextension='.png')
+               screenshots.save(file_path)
+               break
+        else:
+            pass
+    except:
+        break
+
+ 
