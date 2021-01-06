2e022e81561c9efb973553f5f13f56f5cd5db05b
Vaibhav Singhal <iamvs2002@gmail.com>
2021-01-06 22:03:49 +0530

    Update screenshottaker.java

--- Java/ScreenshotTaker/screenshottaker.java
+++ Java/ScreenshotTaker/screenshottaker.java
@@ -7,18 +7,24 @@ import java.awt.Rectangle;
 import java.awt.Toolkit; 
 import java.awt.Robot; 
 import javax.imageio.ImageIO; 
+import java.util.*;
 
 public class screenshottaker { 
     public static final long serialVersionUID = 1L; 
+    
+    private static Scanner sc = new Scanner(System.in);
+    
     public static void main(String[] args) 
     { 
+        System.out.println("Kindly enter the name of the file: ");
+        String name = sc.next();
         try 
         {
             Thread.sleep(120); //sleeping the main thread for 120 ms
             Robot r = new Robot(); 
 
             //to save the screen shot to a specific location
-            String path = "D:// Screenshot.jpg";
+            String path = "D:// "+name+".jpg";
             
             //Getting the screen size
             Rectangle capture =  new Rectangle(Toolkit.getDefaultToolkit().getScreenSize()); 
@@ -33,3 +39,13 @@ public class screenshottaker {
         } 
     } 
 } 
+
+/*
+Output:
+
+Kindly enter the name of the file: 
+mydear
+Screenshot saved
+
+*/
+
