0dabfdfad3115c5024f7819741c110edc5b6a59c
aryangulati <42711978+aryangulati@users.noreply.github.com>
2021-01-18 21:25:23 +0530

    PDF Reader Using Python #129

---
+++ Python/PdfReader/pdf_reader.py
@@ -0,0 +1,23 @@
+import pyttsx3 #python text to speech version 3
+import PyPDF2 
+
+pdf= open("Andriy_Burkov.pdf",'rb') #you can replace this with any pdf file 
+
+pdfreader = PyPDF2.PdfFileReader(pdf)
+
+pages=pdfreader.numPages 
+print(pages)
+
+
+dell= pyttsx3.init() #intialising object 
+
+pages=pdfreader.getPage(7)
+
+text=pages.extractText() #extract text from pages 
+
+# dell.say("Hello How Can I help you")
+dell.say(text)
+
+dell.runAndWait()
+
+#you can read documentation here for pyttsx3 : https://pypi.org/project/pyttsx3/
