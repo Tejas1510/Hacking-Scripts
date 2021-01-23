51d13477b97e0c351fdc287e20681f3bd8fd2969
Your Name <7saikat7.com>
2021-01-24 02:38:34 +0530

    Whatsapp automation

--- Python/Bulk_message_whatsapp/whatsapp.py
+++ Python/Bulk_message_whatsapp/whatsapp.py
@@ -1,23 +1,7 @@
-import pyautogui as pg
-import webbrowser as web
-import time
-import pandas as pd
-#data = pd.read_csv("check.csv")
-data = pd.read_excel(r"Hacking-Scripts\Python\Bulk_message_whatsapp\check2.xlsx")
-data_dict = data.to_dict('list')
-leads = data_dict['LeadNumber']
-messages = data_dict['Message']
-combo = zip(leads,messages)
-first = True
-for lead,message in combo:
-    time.sleep(4)
-    web.open("https://web.whatsapp.com/send?phone="+lead+"&text="+message)
-    if first:
-        time.sleep(6)
-        first=False
-    width,height = pg.size()
-    pg.click(width/2,height/2)
-    time.sleep(12)
-    pg.press('enter')
-    time.sleep(12)
-    pg.hotkey('ctrl', 'w')
+import pywhatkit
+p=input("Enter the number with country code eg.'+91*********' ,Don't Forget to add the country code with a + :")
+q=input("PLease enter the message you want to send to the receiver : ")
+Time=int(input('Enter the Hour : '))
+Time2=int(input('Enter the Minutes : '))
+
+pywhatkit.sendwhatmsg(p,q,Time,Time2)
