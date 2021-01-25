9ff493c3e68c4261b64646c5258eb7bfdeabf57e
Your Name <7saikat7.com>
2021-01-26 00:53:12 +0530

    adding images in readme

--- Python/whatsapp_automation/whatsapp.py
+++ Python/whatsapp_automation/whatsapp.py
@@ -1,7 +1,7 @@
 import pywhatkit
 p=input("Enter the number with country code eg.'+91*********' ,Don't Forget to add the country code with a + :")
 q=input("PLease enter the message you want to send to the receiver : ")
-Time=int(input('Enter the Hour : '))
+Time=int(input('Enter the Hour in +12 format e.g : 8pm=20 '))
 Time2=int(input('Enter the Minutes : '))
 
 pywhatkit.sendwhatmsg(p,q,Time,Time2)
