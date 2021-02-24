1bcffda0d4d7f59557f6d06ebbe8b7dce9419dda
noor <noortshepo@gmail.com>
2021-02-24 16:41:01 +0200

    added images for Java, Reactjs, C and Android Studio scripts

--- Project Website/assets/js/main.js
+++ Project Website/assets/js/main.js
@@ -193,17 +193,152 @@ request.onload = function(){
 request.send();
 */
 
+//an array of objects containing links to images
+const image_urls = [
+  {
+    name: "Calculator",
+    link: "https://images.unsplash.com/photo-1612622826776-cd8b6da23632?ixid=MXwxMjA3fDB8MHxzZWFyY2h8Mzh8fGNhbGN1bGF0b3J8ZW58MHx8MHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60"
+  },
+  {
+    name: "CurrencyConverter",
+    link: "https://images.unsplash.com/photo-1563986768711-b3bde3dc821e?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MTV8fGN1cnJlbmN5JTIwZXhjaGFuZ2V8ZW58MHx8MHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60"
+  },
+  {
+    name: "DiceRoller",
+    link: "https://images.unsplash.com/photo-1551431009-a802eeec77b1?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MXx8ZGljZXxlbnwwfHwwfA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60"
+  },
+  {
+    name: "DrawingApp",
+    link: "https://images.unsplash.com/photo-1524642176501-f3393ec0b116?ixid=MXwxMjA3fDB8MHxzZWFyY2h8Mjl8fGRyYXd8ZW58MHx8MHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60"
+  },
+  {
+    name: "GuessTheNumberGame",
+    link: "https://images.unsplash.com/photo-1502570149819-b2260483d302?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MXx8bnVtYmVyc3xlbnwwfHwwfA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60"
+  },
+  {
+    name: "Memer",
+    link: "https://images.unsplash.com/photo-1607798421660-7d2c0bcff934?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MjF8fG1lbWV8ZW58MHx8MHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60"
+  },
+  {
+    name: "Random Password Generator",
+    link: "https://images.unsplash.com/photo-1504203700686-f21e703e5f1c?ixid=MXwxMjA3fDB8MHxzZWFyY2h8Mnx8cGFzc3dvcmR8ZW58MHx8MHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60"
+  },
+  {
+    name: "TicTacToe",
+    link: "https://images.unsplash.com/photo-1608111282283-df105d21a7ce?ixid=MXwxMjA3fDB8MHxzZWFyY2h8OHx8dGljJTIwdGFjfGVufDB8fDB8&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60"
+  },
+  {
+    name: "Network_Scanner",
+    link: "https://images.unsplash.com/photo-1520869562399-e772f042f422?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MTB8fG5ldHdvcmt8ZW58MHx8MHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60"
+  },
+  {
+    name: "RandomPasswordsGenerator",
+    link: "https://images.unsplash.com/photo-1526374965328-7f61d4dc18c5?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MTB8fHBhc3N3b3JkfGVufDB8fDB8&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60"
+  },
+  {
+    name: "BitcoinPriceSimulator",
+    link: "https://images.unsplash.com/photo-1543699565-003b8adda5fc?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MTZ8fGJpdGNvaW58ZW58MHx8MHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60"
+  },
+  {
+    name: "Currency-converter",
+    link: "https://images.unsplash.com/photo-1563986768711-b3bde3dc821e?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MTV8fGN1cnJlbmN5JTIwZXhjaGFuZ2V8ZW58MHx8MHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60"
+  },
+  {
+    name: "Github-Data-Fetcher",
+    link: "https://images.unsplash.com/photo-1556075798-4825dfaaf498?ixid=MXwxMjA3fDB8MHxzZWFyY2h8N3x8Z2l0aHVifGVufDB8fDB8&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60"
+  },
+  // {
+  //   name: "markdown-previewer",
+  //   link: ""
+  // },
+  {
+    name: "qr-code-generator",
+    link: "https://images.unsplash.com/photo-1595079676339-1534801ad6cf?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MTV8fHFyJTIwY29kZXxlbnwwfHwwfA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60"
+  },
+  {
+    name: "sort-visualizer",
+    link: "https://images.unsplash.com/photo-1591696205602-2f950c417cb9?ixid=MXwxMjA3fDB8MHxzZWFyY2h8Mnx8Y2hhcnR8ZW58MHx8MHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60"
+  },
+  {
+    name: "AutomatingAmazon",
+    link: "https://images.unsplash.com/photo-1523474253046-8cd2748b5fd2?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MXx8YW1hem9ufGVufDB8fDB8&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60"
+  },
+  {
+    name: "CompleteIPAddressDetailsFinder",
+    link: "https://images.unsplash.com/photo-1590233033196-a50b826b059b?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MXx8bWFwfGVufDB8fDB8&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60"
+  },
+  // {
+  //   name: "DirectoryLister",
+  //   link: ""
+  // },
+  // {
+  //   name: "DuplicateFileRemover",
+  //   link: ""
+  // },
+  {
+    name: "Guessing Game IN JAVA",
+    link: "https://images.unsplash.com/photo-1588591795084-1770cb3be374?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MjB8fGdhbWV8ZW58MHx8MHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60"
+  },
+  {
+    name: "MorseCode Generator",
+    link: "https://images.unsplash.com/photo-1514820402329-de527fdd2e6d?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MTN8fG1vcnNlJTIwY29kZXxlbnwwfHwwfA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60"
+  },
+  {
+    name: "NumberGuessingGame",
+    link: "https://images.unsplash.com/photo-1502570149819-b2260483d302?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MXx8bnVtYmVyc3xlbnwwfHwwfA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60"
+  },
+  {
+    name: "PDF-Encrypter",
+    link: "https://images.unsplash.com/photo-1477039181047-efb4357d01bd?ixid=MXwxMjA3fDB8MHxzZWFyY2h8M3x8ZW5jcnlwdHxlbnwwfHwwfA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60"
+  },
+  {
+    name: "RandomPasswordGenerator",
+    link: "https://images.unsplash.com/photo-1504203700686-f21e703e5f1c?ixid=MXwxMjA3fDB8MHxzZWFyY2h8Mnx8cGFzc3dvcmR8ZW58MHx8MHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60"
+  },
+  {
+    name: "RollDice",
+    link: "https://images.unsplash.com/photo-1522069213448-443a614da9b6?ixid=MXwxMjA3fDB8MHxzZWFyY2h8OXx8ZGljZXxlbnwwfHwwfA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60"
+  },
+  {
+    name: "ScreenshotTaker",
+    link: "https://images.unsplash.com/photo-1566241477600-ac026ad43874?ixid=MXwxMjA3fDB8MHxzZWFyY2h8Mnx8c2NyZWVuc2hvdHxlbnwwfHwwfA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60"
+  },
+  {
+    name: "SizeOfDirectory",
+    link: "https://images.unsplash.com/photo-1589995186011-a7b485edc4bf?ixid=MXwxMjA3fDB8MHxzZWFyY2h8Mnx8aGRkfGVufDB8fDB8&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60"
+  },
+  {
+    name: "Github-Data-Fetcher",
+    link: ""
+  },
+]
+
+const default_image = {
+  name: "default",
+  link: "assets/img/services.jpg"
+}
+
+// assets/img/services.jpg - default image, dont lose it
+
 function fetchDataAndroid() {
   fetch('https://api.github.com/repos/Tejas1510/Hacking-Scripts/contents/Android Studio Scripts').then(response => {
     return response.json();
   })
     .then(data => {
       const html = data.map(script => {
+
+        //find the image that correspond with the script
+        let image_url = image_urls.find(image => image.name === script.name);
+        if (!image_url) {
+          //if there is no image assigned for the script, set a default image
+          image_url = default_image
+        }
+
         return `
         <div class="col-md-6 d-flex align-items-stretch" data-aos="fade-up">
         <div class="card">
           <div class="card-img">
-            <img src="assets/img/services.jpg" alt="...">
+            <img src="${image_url.link}" alt="...">
           </div>
           <div class="card-body">
             <h4 class="card-title"><a href="#">${script.name}</a></h4>
@@ -232,11 +367,19 @@ function fetchDataC() {
   })
     .then(data => {
       const html = data.map(script => {
+        //find the image that correspond with the script
+        let image_url = image_urls.find(image => image.name === script.name);
+        if (!image_url) {
+          //if there is no image assigned for the script, set a default image
+          image_url = default_image
+        }
+
+
         return `
           <div class="col-md-6 d-flex align-items-stretch" data-aos="fade-up">
           <div class="card">
             <div class="card-img">
-              <img src="assets/img/aboutus.jpg" alt="...">
+              <img src="${image_url.link}" alt="...">
             </div>
             <div class="card-body">
               <h4 class="card-title"><a href="#">${script.name}</a></h4>
@@ -266,12 +409,20 @@ function fetchDataJava() {
   })
     .then(data => {
       const html = data.map(script => {
+
+        //find the image that correspond with the script
+        let image_url = image_urls.find(image => image.name === script.name);
+        if (!image_url) {
+          //if there is no image assigned for the script, set a default image
+          image_url = default_image
+        }
+
         if (script.name != "bin" && script.name != ".classpath" && script.name != ".project") {
           return `
         <div class="col-md-6 d-flex align-items-stretch" data-aos="fade-up">
         <div class="card">
         <div class="card-img">
-        <img src="assets/img/services.jpg" alt="...">
+        <img src="${image_url.link}" alt="...">
       </div>
           <div class="card-body">
             <h4 class="card-title"><a href="#">${script.name}</a></h4>
@@ -304,12 +455,21 @@ function fetchDataJavaScript() {
     .then(data => {
 
       const html = data.map(script => {
+
+        //find the image that correspond with the script
+        let image_url = image_urls.find(image => image.name === script.name);
+        if (!image_url) {
+          //if there is no image assigned for the script, set a default image
+          image_url = default_image
+        }
+
+
         // console.log(script);
         return `
         <div class="col-md-6 d-flex align-items-stretch" data-aos="fade-up">
         <div class="card">
           <div class="card-img">
-            <img src="assets/img/aboutus.jpg" alt="...">
+            <img src="${image_url.link}" alt="...">
           </div>
           <div class="card-body">
             <h4 class="card-title"><a href="#">${script.name}</a></h4>
@@ -341,11 +501,19 @@ function fetchDataPython() {
   })
     .then(data => {
       const html = data.map(script => {
+
+        //find the image that correspond with the script
+        let image_url = image_urls.find(image => image.name === script.name);
+        if (!image_url) {
+          //if there is no image assigned for the script, set a default image
+          image_url = default_image
+        }
+
         return `
         <div class="col-md-6 d-flex align-items-stretch" data-aos="fade-up">
         <div class="card">
           <div class="card-img">
-            <img src="assets/img/services.jpg" alt="...">
+            <img src="${image_url.link}" alt="...">
           </div>
           <div class="card-body">
             <h4 class="card-title"><a href="#">${script.name}</a></h4>
@@ -375,11 +543,19 @@ function fetchDataReactJs() {
   })
     .then(data => {
       const html = data.map(script => {
+
+        //find the image that correspond with the script
+        let image_url = image_urls.find(image => image.name === script.name);
+        if (!image_url) {
+          //if there is no image assigned for the script, set a default image
+          image_url = default_image
+        }
+
         return `
           <div class="col-md-6 d-flex align-items-stretch" data-aos="fade-up">
           <div class="card">
             <div class="card-img">
-              <img src="assets/img/aboutus.jpg" alt="...">
+              <img src="${image_url.link}" alt="...">
             </div>
             <div class="card-body">
               <h4 class="card-title"><a href="#">${script.name}</a></h4>
