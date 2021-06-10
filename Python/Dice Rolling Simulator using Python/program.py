5c3d3d0917f7ae1f8993d98f717de95a4eeaf8ec
Anant chauhan <63709820+Anant1711@users.noreply.github.com>
2021-06-10 11:00:34 +0530

    Update Dice rolling simulator using python

--- Python/Dice Rolling Simulator using Python/program.py
+++ Python/Dice Rolling Simulator using Python/program.py
@@ -1,5 +1,5 @@
 import random
-
+import time
 x = "y"
 
 while x == "y":
@@ -7,44 +7,56 @@ while x == "y":
     # Gnenerates a random number
     # between 1 and 6 (including
     # both 1 and 6)
+    str = "Rolling..."
+    strp = str.center(93)
+    print("\n")
+    print(strp)
+    print("\n")
+    time.sleep(0.5)
     no = random.randint(1, 6)
 
     if no == 1:
-        print("[-----]")
-        print("[	 ]")
-        print("[ 0 ]")
-        print("[	 ]")
-        print("[-----]")
+        print("\t\t\t\t\t __________")
+        print("\t\t\t\t\t|          |")
+        print("\t\t\t\t\t|          |")
+        print("\t\t\t\t\t|    0     |")
+        print("\t\t\t\t\t|          |")
+        print("\t\t\t\t\t|__________|") 
     if no == 2:
-        print("[-----]")
-        print("[ 0 ]")
-        print("[	 ]")
-        print("[ 0 ]")
-        print("[-----]")
+        print("\t\t\t\t\t __________")
+        print("\t\t\t\t\t|          |")
+        print("\t\t\t\t\t|        0 |")
+        print("\t\t\t\t\t|          |")
+        print("\t\t\t\t\t|0         |")
+        print("\t\t\t\t\t|__________|")
     if no == 3:
-        print("[-----]")
-        print("[	 ]")
-        print("[0 0 0]")
-        print("[	 ]")
-        print("[-----]")
+        print("\t\t\t\t\t __________")
+        print("\t\t\t\t\t|          |")
+        print("\t\t\t\t\t|        0 |")
+        print("\t\t\t\t\t|    0     |")
+        print("\t\t\t\t\t|0         |")
+        print("\t\t\t\t\t|__________|")
     if no == 4:
-        print("[-----]")
-        print("[0 0]")
-        print("[	 ]")
-        print("[0 0]")
-        print("[-----]")
+        print("\t\t\t\t\t __________")
+        print("\t\t\t\t\t|          |")
+        print("\t\t\t\t\t|0       0 |")
+        print("\t\t\t\t\t|          |")
+        print("\t\t\t\t\t|0       0 |")
+        print("\t\t\t\t\t|__________|")
     if no == 5:
-        print("[-----]")
-        print("[0 0]")
-        print("[ 0 ]")
-        print("[0 0]")
-        print("[-----]")
+        print("\t\t\t\t\t __________")
+        print("\t\t\t\t\t|          |")
+        print("\t\t\t\t\t|0       0 |")
+        print("\t\t\t\t\t|    0     |")
+        print("\t\t\t\t\t|0       0 |")
+        print("\t\t\t\t\t|__________|")
     if no == 6:
-        print("[-----]")
-        print("[0 0 0]")
-        print("[	 ]")
-        print("[0 0 0]")
-        print("[-----]")
+        print("\t\t\t\t\t __________")
+        print("\t\t\t\t\t|          |")
+        print("\t\t\t\t\t|0       0 |")
+        print("\t\t\t\t\t|0       0 |")
+        print("\t\t\t\t\t|0       0 |")
+        print("\t\t\t\t\t|__________|")
 
-    x = input("press y to roll again and n to exit:")
+    x = input("Press (y) to roll again and for exit press any other key: ")
     print("\n")
