5763d710927867c3102d2ea9547feb609b6fba64
Drishti Rana <81031606+dishi24@users.noreply.github.com>
2021-06-13 08:37:10 +0530

    Create Treasure Island.py
    This is a Treasure Island game made simply by using if else commands

---
+++ Python/Treasure Island.py
@@ -0,0 +1,47 @@
+print('''
+*******************************************************************************
+          |                   |                  |                     |
+ _________|________________.=""_;=.______________|_____________________|_______
+|                   |  ,-"_,=""     `"=.|                  |
+|___________________|__"=._o`"-._        `"=.______________|___________________
+          |                `"=._o`"=._      _`"=._                     |
+ _________|_____________________:=._o "=._."_.-="'"=.__________________|_______
+|                   |    __.--" , ; `"=._o." ,-"""-._ ".   |
+|___________________|_._"  ,. .` ` `` ,  `"-._"-._   ". '__|___________________
+          |           |o`"=._` , "` `; .". ,  "-._"-._; ;              |
+ _________|___________| ;`-.o`"=._; ." ` '`."\` . "-._ /_______________|_______
+|                   | |o;    `"-.o`"=._``  '` " ,__.--o;   |
+|___________________|_| ;     (#) `-.o `"=.`_.--"_o.-; ;___|___________________
+____/______/______/___|o;._    "      `".o|o_.--"    ;o;____/______/______/____
+/______/______/______/_"=._o--._        ; | ;        ; ;/______/______/______/_
+____/______/______/______/__"=._o--._   ;o|o;     _._;o;____/______/______/____
+/______/______/______/______/____"=._o._; | ;_.--"o.--"_/______/______/______/_
+____/______/______/______/______/_____"=.o|o_.--""___/______/______/______/____
+/______/______/______/______/______/______/______/______/______/______/_____ /
+*******************************************************************************
+''')
+print("Welcome to Treasure Island.")
+print("Your mission is to find the treasure.") 
+
+#https://www.draw.io/?lightbox=1&highlight=0000ff&edit=_blank&layers=1&nav=1&title=Treasure%20Island%20Conditional.drawio#Uhttps%3A%2F%2Fdrive.google.com%2Fuc%3Fid%3D1oDe4ehjWZipYRsVfeAx2HyB7LCQ8_Fvi%26export%3Ddownload
+
+print("You are standing on a crossroad")
+turn = input("Where you want to turn? right or left?  ")
+ 
+if turn == "right" :
+  print("There is a river infront of you.")
+  dec = input("Wait for a boat to carry accross or Swim by yourself? Wait or Swim?  ")
+  if dec == "wait":
+    print ("You have crossed the river. Now there are two boxes infront of you blue and green.")
+    dec2 = input("Choose one. B or G?  ")
+    if dec2 == "g":
+      print ("Yayyy you found the treasure. Congrats!!")
+    else:
+      print ("There was crying gas inside it")
+      print ("You loose. Game over!") 
+  else:
+    print ("Oh no the alligator attacked :( ")
+    print ("You loose. Game over!")
+else:
+  print ("There was a small hole at your left.")
+  print ("You loose. Game over!")
