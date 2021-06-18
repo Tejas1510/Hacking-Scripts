99c5634e030b337a5bcc3abe9f7de34355fdc372
ArshWalker <arshwalker7@gmail.com>
2021-06-18 15:25:46 +0530

    index.js updated

--- Javascript/Analog Clock/index.js
+++ Javascript/Analog Clock/index.js
@@ -1,14 +1,26 @@
+const toggle = document.querySelector(".toggle");
+toggle.addEventListener("click", (e) => {
+  const html = document.querySelector("html");
+  if (html.classList.contains("dark")) {
+    html.classList.remove("dark");
+    e.target.innerHTML = "Dark mode";
+  } else {
+    html.classList.add("dark");
+    e.target.innerHTML = "Light mode";
+  }
+});
+
 setInterval(() => {
-    d = new Date();
-    htime = d.getHours();
-    mtime = d.getMinutes();
-    stime = d.getSeconds();
+  d = new Date();
+  htime = d.getHours();
+  mtime = d.getMinutes();
+  stime = d.getSeconds();
 
-    hrotation = 30 * htime + mtime / 2;
-    mrotation = 6 * mtime;
-    srotation = 6 * stime;
+  hrotation = 30 * htime + mtime / 2;
+  mrotation = 6 * mtime;
+  srotation = 6 * stime;
 
-    hour.style.transform = `rotate(${hrotation}deg)`;
-    minute.style.transform = `rotate(${mrotation}deg)`;
-    second.style.transform = `rotate(${srotation}deg)`;
+  hour.style.transform = `rotate(${hrotation}deg)`;
+  minute.style.transform = `rotate(${mrotation}deg)`;
+  second.style.transform = `rotate(${srotation}deg)`;
 }, 1000);
