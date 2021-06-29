998b1a90960af31adc311802c81a38dde54761f1
Akash Ramanand Rajak <aakashrajak02@gmail.com>
2021-06-29 23:05:45 +0530

    Removed unnecessary comments and added new commments

--- Python/Direct Flight Route Finder/direct_flight_route_finder.py
+++ Python/Direct Flight Route Finder/direct_flight_route_finder.py
@@ -7,12 +7,12 @@ import tkinter as tk
 import tkinter.messagebox as mbox
 import pandas as pd
 
-
+# created main window
 window = Tk()
 window.geometry("1000x700")
 window.title("Direct Flight Route Finder")
 
-# ---------------------------------------------------------
+# ------------------- for adding gif image in main winfdow --------------------------------------
 frameCnt = 3
 frames = [PhotoImage(file='Images/flight.gif',format = 'gif -index %i' %(i)) for i in range(frameCnt)]
 
@@ -33,18 +33,14 @@ label.place(x = 220, y = 100)
 window.after(0, update, 0)
 # --------------------------------------------------------------------
 
+# read the data from the csv file
 data = pd.read_csv("flight1z.csv")
 source_airport_list = data["source_airport"].tolist()
 source_airport_id_list = data["source_airport_id"].tolist()
 destination_airport_list = data["destination_airport"].tolist()
 destination_airport_id_list = data["destination_airport_id"].tolist()
 
-# print(source_airport_list)
-# print(source_airport_id_list)
-# print(destination_airport_list)
-# print(destination_airport_id_list)
-
-
+# function to find if route exist
 def find_route():
     src = first_var.get()
     dest = second_var.get()
@@ -100,22 +96,25 @@ second_menu["menu"].config(font=("Arial", 10), bg = "light yellow", fg = "blue")
 second_menu.place(x=650, y=500)
 second_var.set("KZN") # size 1 is selected as by default, and we can
 
-
+# created find button
 findb = Button(window, text="FIND",command=find_route,font=("Arial", 20), bg = "light green", fg = "blue", borderwidth=3, relief="raised")
 findb.place(x =200 , y =600 )
 
+# function to reset the selected value
 def reset_label():
     first_var.set("AER")
     second_var.set("KZN")
 
+# created reset button
 resetb = Button(window, text="RESET",command=reset_label,font=("Arial", 20), bg = "light green", fg = "blue", borderwidth=3, relief="raised")
 resetb.place(x =420 , y =600 )
 
-
+# function for exiting
 def exit_win():
     if mbox.askokcancel("Exit", "Do you want to exit?"):
         window.destroy()
 
+# created exit button
 exitb = Button(window, text="EXIT",command=exit_win,font=("Arial", 20), bg = "red", fg = "blue", borderwidth=3, relief="raised")
 exitb.place(x =660 , y =600 )
 
